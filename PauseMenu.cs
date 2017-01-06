9e4f4174b92d7eca63e18121b63b0de71cf82b17
CtrlPwnDelete <jakezeiner@gmail.com>
2017-01-06 14:05:24 -0600

    Add files via upload

---
+++ PauseMenu.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+/// <summary>
+/// This script pauses the game when you press the escape key.
+/// </summary>
+
+public class PauseMenu : MonoBehaviour {
+	
+	public GameObject PauseUI;
+	
+	private bool paused = false;
+
+	public GameObject MainMenuUI;
+	
+	void Start () {
+
+		MainMenuUI = GameObject.Find ("Main Menu");
+		MainMenuUI.SetActive (false);
+
+		//makes sure that the game is not paused on startup
+		paused = false;
+
+		//finds the pause menu
+		PauseUI = GameObject.Find ("Pause Menu");
+	}
+	
+	void Update () {
+
+		//pauses the game when the escape key is pressed
+		if (Input.GetKeyDown (KeyCode.Escape))
+		{
+			paused = !paused;
+			MainMenuUI.SetActive (false);
+		}
+		
+		if (paused)
+		{
+			PauseUI.SetActive (true);
+			Time.timeScale = 0;
+		}
+		
+		else if (!paused)
+		{
+			PauseUI.SetActive (false);
+			Time.timeScale = 1;
+		}
+		
+	}
+
+	//resumes the game on button click
+	public void Resume()
+	{
+		paused = false;
+		
+	}
+
+	//restarts the level on button click
+	public void Restart()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	//loads the main menu and closes the level on button click
+	public void MainMenu()
+	{
+		MainMenuUI.SetActive (true);
+	}
+
+	//closes the game on button click
+	public void Exit ()
+	{
+		Application.Quit();
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//require this script for the character controller
+[RequireComponent (typeof (CharacterController))]
+
+//Adds the script to the components menu
+[AddComponentMenu ("Control Script/PlayerController")]
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 6.0f;
+	public float gravity = 20.0f;
+	public float jumpSpeed = 8.0f;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	private Rigidbody rb;
+
+	private CharacterController controller;
+	
+	void Start()
+	{
+		//Store reference to attached component
+		controller = GetComponent<CharacterController>( );
+	}
+	
+	void Update()
+	{
+		if (controller.isGrounded)
+		{
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			
+			if (Input.GetButtonDown ("Jump"))
+			{
+				moveDirection.y = jumpSpeed;
+			}
+		}
+	
+		moveDirection.y -= gravity * Time.deltaTime;
+
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
