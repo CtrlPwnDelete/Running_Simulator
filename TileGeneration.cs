7dec8a165c5451a022bf15a7a36ed9d794638988
Kinghawk159 <littlezeeman@gmail.com>
2017-01-17 13:40:52 -0600

    Add files via upload
    tiles

---
+++ TileGeneration.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileGeneration : MonoBehaviour {
+	
+	public GameObject C;
+
+	public GameObject[] tilePrefabs;
+	int i = 0;
+	bool test = true;
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+	public void SpawnTile()
+	{
+		for(i = 0; i < 1; i++)
+		{
+			C = (GameObject)Instantiate(tilePrefabs[0], C.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
+
+		}
+
+	}
+
+
+
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		SpawnTile();
+	}
+}
