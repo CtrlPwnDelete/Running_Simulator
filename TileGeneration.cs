9528251abcd14cb78e7fa62382e265f90fac4df9
Kinghawk159 <littlezeeman@gmail.com>
2017-02-02 13:28:56 -0600

    Add files via upload
    FINISHED

--- TileGeneration.cs
+++ TileGeneration.cs
@@ -1,30 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Timers;
 
 public class TileGeneration : MonoBehaviour {
 	
 	public GameObject C;
+	public GameObject L;
+	public GameObject R;
 
 	public GameObject[] tilePrefabs;
-	int i = 0;
-	bool test = true;
+	int number;
+	Vector3 oldPosition;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		oldPosition = new Vector3 (0, 0.5f, -3.31f);
 
 	}
 
 	public void SpawnTile()
 	{
-		for(i = 0; i < 1; i++)
-		{
-			C = (GameObject)Instantiate(tilePrefabs[0], C.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
+		number = Random.Range (0,3);
 
+
+		if(number == 0)
+		{
+
+		tilePrefabs[number] = (GameObject)Instantiate(tilePrefabs[number], oldPosition, Quaternion.identity);
+	
 		}
 
+		if(number == 1)
+		{
+
+			tilePrefabs[number] = (GameObject)Instantiate(tilePrefabs[number], oldPosition, Quaternion.identity);
+
+				
+		}
+		if(number == 2)
+		{
+			
+			tilePrefabs[number] = (GameObject)Instantiate(tilePrefabs[number], oldPosition, Quaternion.identity);
+		
+		}
+
+		oldPosition = new Vector3 (oldPosition.x, oldPosition.y, oldPosition.z + 4.5f); 
+
 	}
-
-
+	
 
 
 
@@ -32,6 +56,8 @@ public class TileGeneration : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		SpawnTile();
+
 	}
 }
