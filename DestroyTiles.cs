8c11dc716208a395da607178ee14ba07fcae6c56
Kinghawk159 <littlezeeman@gmail.com>
2017-02-21 14:08:12 -0600

    Tile Destroy

---
+++ DestroyTiles.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DestroyTiles : MonoBehaviour
+{
+	/// <summary>
+	/// This allows the tiles to be destroyed.
+	/// </summary>
+
+
+	void Start () {
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log (other);
+		if (other.gameObject.tag == "Tile") {
+			Destroy(other.gameObject);
+
+		} 
+	
+	}
+
+}
