68a0601393caa5502ad768b64546b4636702bab4
CtrlPwnDelete <jakezeiner@gmail.com>
2017-02-21 12:57:56 -0600

    Add files via upload

--- CollectItems.cs
+++ CollectItems.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CollectItems : MonoBehaviour
@@ -6,46 +7,89 @@ public class CollectItems : MonoBehaviour
 	/// <summary>
 	/// This allows the character to pick up items and increase score and highscore.
 	/// </summary>
-
-
-	public int highscore = 0;
-	public int score = 0;
+	
+	
+	private float highscore;
+	private float score;
 	GameObject player;
 
-	void OnTriggerEnter(Collider other){
-		Destroy(other.gameObject);
+	public Text scoreText;
+	public Text highscoreText;
+
+	void Start () {
+		UpdateScore ();
 	}
 
-	//This method sees if the player collides with a Fruit and if it does, it will change the score
-	void OnCollisionEnter (Collision col)
+	void OnTriggerEnter(Collider other)
 	{
-		if (col.gameObject.name == "Fruit") {
+		if (other.name == "Apple") {
 			score += 50;
-		}
-		if (col.gameObject.name == "Fruit") {
-			score += 100;
-		}
-		if (col.gameObject.name == "Fruit") {
-			score += 250;
-		}
-	}
+			Destroy(other.gameObject);
+		} 
 
-	//this method updates the highscore if the players score is higher than the high score
-	public void updateHighscore ()
-	{
-		if (score > highscore) {
-			highscore = score;
+		else if (other.name == "Blue Berry")
+		{
+			score += 100;
+			Destroy(other.gameObject);
 		}
+
+		else if (other.name == "Banana")
+		{
+			score += 150;
+			Destroy(other.gameObject);
+		}
+
+		else if (other.name == "Orange")
+		{
+			score += 200;
+			Destroy(other.gameObject);
+		}
+
+		else if (other.name == "Peach")
+		{
+			score += 250;
+			Destroy(other.gameObject);
+		}
+
+		else if (other.name == "Strawberry")
+		{
+			score += 300;
+			Destroy(other.gameObject);
+		}
+
+		else if (other.name == "Corn")
+		{
+			score += 300;
+			Destroy(other.gameObject);
+		}
+
+		else if (other.name == "Pommegranate")
+		{
+			score += 350;
+			Destroy(other.gameObject);
+		}
+		UpdateScore (); 
 	}
 
 	void Update ()
 	{
-		updateHighscore ();
-		Debug.Log (score);
+		if (score > highscore)
+		{
+			highscore = score;
+		}
 
+		Debug.Log (highscore);
 	}
 
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
 
+		if (score > highscore)
+		{
+			highscore = score;
+		}
 
-
+		highscoreText.text = "High Score: " + highscore;
+	}
 }
